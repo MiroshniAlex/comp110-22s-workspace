004d4ccf7c812c73bf6cbc6c67d891bc364b1bed
Alexander Miroshnichenko <amiroshnichenko@unc.edu>
2022-01-29 15:10:50 -0500

    Fixed Execrise 2 Bugs

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -14,24 +14,24 @@ player_guess: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
 # checks format of guess
 while len(player_guess) != len(secret_word):
-    player_guess: str = input(f"That was not {len(secret_word)} letters! Try again: ")
+    player_guess = input(f"That was not {len(secret_word)} letters! Try again: ")
 
 # begins checking guess for correct letters
 while guess_index < len(secret_word):
     secret_word_index = 0
     incorrect_pos: bool = False
     if player_guess[guess_index] == secret_word[guess_index]:
-        emoji_hint += f"{GREEN_BOX} "
+        emoji_hint += f"{GREEN_BOX}"
     else:
         # This while loop checks if the secret letter in the currect guess index position matches any other letters in the guess
         while secret_word_index < len(secret_word):
-            if player_guess[secret_word_index] == secret_word[guess_index]:
+            if player_guess[guess_index] == secret_word[secret_word_index]:
                 incorrect_pos = True
             secret_word_index += 1
         if incorrect_pos is True:
-            emoji_hint += f"{YELLOW_BOX} " 
+            emoji_hint += f"{YELLOW_BOX}" 
         else:
-            emoji_hint += f"{WHITE_BOX} "
+            emoji_hint += f"{WHITE_BOX}"
     guess_index += 1
 
 print(emoji_hint)
